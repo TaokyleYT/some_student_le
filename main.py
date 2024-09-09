dd6f3424efe26f541c986fffeab69a29194a33d7
TaokyleYT <102730210+TaokyleYT@users.noreply.github.com>
2024-09-09 06:07:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from some_student_le.really_not_good import *
+import random
+print("hello there! Lets play a game.")
+op = ["+","-","*","/"][random.randint(0, 3)]
+a = random.randint(2,1000)
+if op=="/":
+  b=a*random.randint(2,100)
+elif op=="*":
+  b = random.randint(2,100)
+else:
+  b = random.randint(2,1000)
+ans = input("what is "+str(b)+op+str(a)+"? ")
+if ans == str(eval(str(b)+op+str(a))):
+  print("\x1b[42mYES YOU ARE CORRECT!!!\x1b[m")
+else:
+  print("\x1b[41mNO YOU ARE NOT CORRECT!!! The correct answer is",str(int(eval(str(b)+op+str(a))))+"\x1b[m")
+input("Enter to exit ")
---
+++ replit.nix
@@ -0,0 +1,6 @@
+{ pkgs }: {
+    deps = [
+        pkgs.python311Packages.pip
+        pkgs.cowsay
+    ];
+}
